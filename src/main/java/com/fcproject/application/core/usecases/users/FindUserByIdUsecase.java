ad71e0ef2c1da116600547b7c511a01439cd4c53
caue <caueweber22@gmail.com>
2025-12-07 07:49:35 -0300

    Feat: adicionando fluxo de pesquisa por ID

--- src/main/java/com/fcproject/application/core/usecases/users/FindUserByIdUsecase.java
+++ src/main/java/com/fcproject/application/core/usecases/users/FindUserByIdUsecase.java
@@ -1,4 +1,21 @@
 package com.fcproject.application.core.usecases.users;
 
-public class FindUserByIdUsecase {
+import com.fcproject.application.core.domain.users.UserDomain;
+import com.fcproject.application.ports.inbound.userPorts.FindUserByIdInPort;
+import com.fcproject.application.ports.outbound.UserOutPort;
+
+import java.util.UUID;
+
+public class FindUserByIdUsecase implements FindUserByIdInPort {
+    private final UserOutPort repositoryOut;
+
+    public FindUserByIdUsecase(UserOutPort repositoryOut) {
+        this.repositoryOut = repositoryOut;
+    }
+
+    @Override
+    public UserDomain execute(UUID id) {
+
+        return repositoryOut.findById(id);
+    }
 }
